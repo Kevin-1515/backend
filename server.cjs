c61c889fb5306a068e9d4d93f1b765c30b633457
Kevin-1515 <kevin.camona@udea.edu.co>
2024-05-24 11:19:37 -0500

    automatizacion foraneas y endpoints partidos, equipos y jugadores

--- server.cjs
+++ server.cjs
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use(cors());
 
 let mysql = require("mysql");
-let conexion = mysql.createConnection({
+let conexion = mysql.createConnection({   
     host: "localhost",
     port: "3306",
     database: "planillas",
@@ -16,13 +16,15 @@ let conexion = mysql.createConnection({
     password: "root"
 
 });
-
-
+let idActualPartido;
+let idEquipo1;
+let idEquipo2;
 app.get('/partidos',(req, res)=>{
     console.log("alguien hizo get en la ruta /partidos");
     res.send('Aun no hay partidos');
     
 });
+/**crear partido con equipos completo :D :D :D :D :D */
 
 app.post("/partidos/nuevo", (req, res) => {
     const infoPartido = req.body;
@@ -42,33 +44,51 @@ app.post("/partidos/nuevo", (req, res) => {
 
                 conexion.query(sqlLastInsertId, (error, results) => {
                     if (error) {
-                        console.error("Error al obtener el ID del partido", error);
+                        console.error("Error al obtener el ultimo ID", error);
                         return res.status(500).send("Error interno del servidor");
                     }
 
-                    const idPartido = results[0].last_id;
-                    const valoresEquipo1 = [infoPartido.equipo1, idPartido];
-                    const valoresEquipo2 = [infoPartido.equipo2, idPartido];
+                    idActualPartido= results[0].last_id;
+                    const valoresEquipo1 = [infoPartido.equipo1, idActualPartido];
+                    const valoresEquipo2 = [infoPartido.equipo2, idActualPartido];
 
-                    // Insertar equipo 1
+                    
                     conexion.query(sqlInsertEquipo, valoresEquipo1, (error, results) => {
                         if (error) {
                             console.error("Error al insertar equipo 1", error);
                             return res.status(500).send("Error interno del servidor");
                         }
-
-                        // Insertar equipo 2
-                        conexion.query(sqlInsertEquipo, valoresEquipo2, (error, results) => {
-                            if (error) {
-                                console.error("Error al insertar equipo 2", error);
+                        conexion.query(sqlLastInsertId, (error,results) =>{
+                            if (error){
+                                console.error("Error al obtener el ultimo ID", error);
                                 return res.status(500).send("Error interno del servidor");
                             }
-
-                            res.status(200).send("Partido y equipos insertados correctamente");
-                        });
+                        idEquipo1= results[0].last_id;
+                        console.log("id equipo 1");
+                        console.log(idEquipo1);
+                        }
+                        );
+                        
+                    conexion.query(sqlInsertEquipo, valoresEquipo2, (error, results) => {
+                        if (error) {
+                            console.error("Error al insertar equipo 2", error);
+                            return res.status(500).send("Error interno del servidor");
+                        }
+                        conexion.query(sqlLastInsertId, (error,results) =>{
+                            if (error){
+                                console.error("Error al obtener el ultimo ID", error);
+                                return res.status(500).send("Error interno del servidor");
+                            }
+                        idEquipo2= results[0].last_id;
+                        console.log("id equipo 2");
+                        console.log(idEquipo2);
+                        }
+                        );
+                           res.status(200).send("Partido y equipos insertados correctamente");
                     });
                 });
             });
+        });
         } else {
             res.status(400).send("Debes ingresar el nombre de ambos equipos");
         }
@@ -137,97 +157,43 @@ app.post("/partidos/setn",(req, res)=>{
 
 );
 
-app.post("/partidos/jugadorn",(req,res) => {
+app.post("/partidos/jugadoresn",(req,res) => {
     const infjug = req.body;
-    console.log(req.body);
+    console.log(Object.keys(req.body).length);
     try{
         if(Object.keys(req.body) !== null)
         {
             const sql = "INSERT INTO jugadores(nombre,numero, equipos_idequipo) VALUES (?,?,?)"
-            const valores1e1=[req.body.e1j1,req.body.e1n1,1]
-            const valores2e1=[req.body.e1j2,req.body.e1n2,1]  
-            const valores3e1=[req.body.e1j3,req.body.e1n3,1]  
-            const valores4e1=[req.body.e1j4,req.body.e1n4,1]  
-            const valores5e1=[req.body.e1j5,req.body.e1n5,1]  
-            const valores6e1=[req.body.e1j6,req.body.e1n6,1]  
-            const valores1e2=[req.body.e2j1,req.body.e2n1,2]  
-            const valores2e2=[req.body.e2j2,req.body.e2n2,2]  
-            const valores3e2=[req.body.e2j3,req.body.e2n3,2]  
-            const valores4e2=[req.body.e2j4,req.body.e2n4,2]
-            const valores5e2=[req.body.e2j5,req.body.e2n5,2]
-            const valores6e2=[req.body.e2j6,req.body.e2n6,2]  
-            conexion.query(sql, valores1e1,(error, results)=>{
-                if (error){
-                    console.error("error al insertar jugador", error);
+            const nombresJugEq1=[infjug.Eq1Nom1, infjug.Eq1Nom2, infjug.Eq1Nom3, infjug.Eq1Nom4, infjug.Eq1Nom5, infjug.Eq1Nom6, infjug.Eq1Nom7, infjug.Eq1Nom8, infjug.Eq1Nom9, infjug.Eq1Nom10, infjug.Eq1Nom11, infjug.Eq1Nom12, infjug.Eq1Nom13, infjug.Eq1Nom14];
+            const nombresJugEq2=[infjug.Eq2Nom1, infjug.Eq2Nom2, infjug.Eq2Nom3, infjug.Eq2Nom4, infjug.Eq2Nom5, infjug.Eq2Nom6, infjug.Eq2Nom7, infjug.Eq2Nom8, infjug.Eq2Nom9, infjug.Eq2Nom10, infjug.Eq2Nom11, infjug.Eq2Nom12, infjug.Eq2Nom13, infjug.Eq2Nom14];
+            const numerosJugEq1=[infjug.Eq1Num1, infjug.Eq1Num2, infjug.Eq1Num3, infjug.Eq1Num4, infjug.Eq1Num5, infjug.Eq1Num6, infjug.Eq1Num7, infjug.Eq1Num8, infjug.Eq1Num9, infjug.Eq1Num10, infjug.Eq1Num11, infjug.Eq1Num12, infjug.Eq1Num13, infjug.Eq1Num14];
+            const numerosJugEq2=[infjug.Eq2Num1, infjug.Eq2Num2, infjug.Eq2Num3, infjug.Eq2Num4, infjug.Eq2Num5, infjug.Eq2Num6, infjug.Eq2Num7, infjug.Eq2Num8, infjug.Eq2Num9, infjug.Eq2Num10, infjug.Eq2Num11, infjug.Eq2Num12, infjug.Eq2Num13, infjug.Eq2Num14];
+            
+            for (let i=0; i < infjug.CantJugEq1; i++){
+                const valores= [nombresJugEq1[i], numerosJugEq1[i],idEquipo1];
+                conexion.query(sql,valores,(error,results)=>{
+                    if (error){
+                        console.error("error al insertar jugador", error)
+                    }
+
                 }
+
+                )
             }
-            )
-            conexion.query(sql, valores2e1,(error, results)=>{
-                if (error){
-                    console.error("error al insertar jugador", error);
+            for (let i=0; i < infjug.CantJugEq2; i++){
+                const valores= [nombresJugEq2[i], numerosJugEq2[i],idEquipo2];
+                conexion.query(sql,valores,(error,results)=>{
+                    if (error){
+                        console.error("error al insertar jugador", error)
+                    }
+
                 }
+
+                )
             }
-            )
-            conexion.query(sql, valores3e1,(error, results)=>{
-                if (error){
-                    console.error("error al insertar jugador", error);
-                }
-            }
-            )
-            conexion.query(sql, valores4e1,(error, results)=>{
-                if (error){
-                    console.error("error al insertar jugador", error);
-                }
-            }
-            )
-            conexion.query(sql, valores5e1,(error, results)=>{
-                if (error){
-                    console.error("error al insertar jugador", error);
-                }
-            }
-            )
-            conexion.query(sql, valores6e1,(error, results)=>{
-                if (error){
-                    console.error("error al insertar jugador", error);
-                }
-            }
-            )
-            conexion.query(sql, valores1e2,(error, results)=>{
-                if (error){
-                    console.error("error al insertar jugador", error);
-                }
-            }
-            )
-            conexion.query(sql, valores2e2,(error, results)=>{
-                if (error){
-                    console.error("error al insertar jugador", error);
-                }
-            }
-            )
-            conexion.query(sql, valores3e2,(error, results)=>{
-                if (error){
-                    console.error("error al insertar jugador", error);
-                }
-            }
-            )
-            conexion.query(sql, valores4e2,(error, results)=>{
-                if (error){
-                    console.error("error al insertar jugador", error);
-                }
-            }
-            )
-            conexion.query(sql, valores5e2,(error, results)=>{
-                if (error){
-                    console.error("error al insertar jugador", error);
-                }
-            }
-            )
-            conexion.query(sql, valores6e2,(error, results)=>{
-                if (error){
-                    console.error("error al insertar jugador", error);
-                }
-            }
-            )
+
+            
+
         res.send("jugadores creados");
         res.status(200)
         
@@ -276,7 +242,35 @@ app.post("/partidos/tiempon",(req,res) => {
     
 });
 
-
+app.post("/partidos/sancionn",(req,res) => {
+    const infsancion = req.body;
+    console.log(req.body);
+    try{
+        if(Object.keys(req.body) !== null)
+        {
+            const valores=[req.body.tipoSancion,1,1]
+            const sql = "INSERT INTO sanciones(tipoSancion) VALUES (?)"  
+            conexion.query(sql, valores,(error, results)=>{
+                if (error){
+                    console.error("error al insertar sancion", error);
+                }
+            }
+        )
+        res.send("sancion creada");
+        res.status(200)
+        
+        }
+        else{
+            res.sendStatus(500);
+            console.log("debes ingresar una sancion")
+        }
+    }
+    catch{
+        res.sendStatus(500);
+    }
+    
+    
+});
 
 
 
