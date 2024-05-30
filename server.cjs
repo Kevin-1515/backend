d503aecc57fb1315c1829d0bc0264b790cdfb4e8
Kevin-1515 <kevin.camona@udea.edu.co>
2024-05-30 09:35:10 -0500

    endpoint sets terminado

--- server.cjs
+++ server.cjs
@@ -116,29 +116,19 @@ app.post("/partidos/setn",(req, res)=>{
     const infoSets = req.body;
     try{
         if (Object.keys(req.body) !==null){
-            const valores1 = [req.body.puntosASet1, req.body.puntosBSet1 , 1 ,12];
-            const valores2 = [req.body.puntosASet2, req.body.puntosBSet2 , 2 ,12];
-            const valores3 = [req.body.puntosASet3, req.body.puntosBSet3 , 3 ,12];
-            console.log(valores1);
-            console.log(valores2);
-            console.log(valores3);
+            const valores1 = [infoSets.puntosASet1, infoSets.puntosASet2, infoSets.puntosASet3, infoSets.puntosASet4, infoSets.puntosASet5];
+            const valores2 = [infoSets.puntosBSet1, infoSets.puntosBSet2, infoSets.puntosBSet3, infoSets.puntosBSet4, infoSets.puntosBSet5];
             const sql = "INSERT INTO sets (puntosEqA, puntosEqB, numeroSet, partidos_idpartido) VALUES (?,?,?,?)";
-            conexion.query(sql,valores1,(error, results)=>{
-                if (error){
-                    console.error("error al insertar set", error);
-                }
-            })
-            conexion.query(sql,valores2,(error, results)=>{
-                if (error){
-                    console.error("error al insertar set", error);
-                }
-            })
-            conexion.query(sql,valores3,(error, results)=>{
-                if (error){
-                    console.error("error al insertar set", error);
-                }
-            })
-
+            for (let i =0; i< infoSets.CantSets; i++){
+                const valores = [valores1[i], valores2[i], i+1 , idActualPartido];
+                conexion.query(sql,valores,(error, results)=>{
+                    if (error){
+                        console.error("error al insertar set", error);
+                    }
+                })
+            }
+            
+            
             res.send("sets creados");
             res.status(200);
             console.log("datos insertados",req.body);
@@ -164,10 +154,10 @@ app.post("/partidos/jugadoresn",(req,res) => {
         if(Object.keys(req.body) !== null)
         {
             const sql = "INSERT INTO jugadores(nombre,numero, equipos_idequipo) VALUES (?,?,?)"
-            const nombresJugEq1=[infjug.Eq1Nom1, infjug.Eq1Nom2, infjug.Eq1Nom3, infjug.Eq1Nom4, infjug.Eq1Nom5, infjug.Eq1Nom6, infjug.Eq1Nom7, infjug.Eq1Nom8, infjug.Eq1Nom9, infjug.Eq1Nom10, infjug.Eq1Nom11, infjug.Eq1Nom12, infjug.Eq1Nom13, infjug.Eq1Nom14];
-            const nombresJugEq2=[infjug.Eq2Nom1, infjug.Eq2Nom2, infjug.Eq2Nom3, infjug.Eq2Nom4, infjug.Eq2Nom5, infjug.Eq2Nom6, infjug.Eq2Nom7, infjug.Eq2Nom8, infjug.Eq2Nom9, infjug.Eq2Nom10, infjug.Eq2Nom11, infjug.Eq2Nom12, infjug.Eq2Nom13, infjug.Eq2Nom14];
-            const numerosJugEq1=[infjug.Eq1Num1, infjug.Eq1Num2, infjug.Eq1Num3, infjug.Eq1Num4, infjug.Eq1Num5, infjug.Eq1Num6, infjug.Eq1Num7, infjug.Eq1Num8, infjug.Eq1Num9, infjug.Eq1Num10, infjug.Eq1Num11, infjug.Eq1Num12, infjug.Eq1Num13, infjug.Eq1Num14];
-            const numerosJugEq2=[infjug.Eq2Num1, infjug.Eq2Num2, infjug.Eq2Num3, infjug.Eq2Num4, infjug.Eq2Num5, infjug.Eq2Num6, infjug.Eq2Num7, infjug.Eq2Num8, infjug.Eq2Num9, infjug.Eq2Num10, infjug.Eq2Num11, infjug.Eq2Num12, infjug.Eq2Num13, infjug.Eq2Num14];
+            const nombresJugEq1=[infjug.eq1Nom1, infjug.eq1Nom2, infjug.eq1Nom3, infjug.eq1Nom4, infjug.eq1Nom5, infjug.eq1Nom6, infjug.eq1Nom7, infjug.eq1Nom8, infjug.eq1Nom9, infjug.eq1Nom10, infjug.eq1Nom11, infjug.eq1Nom12, infjug.eq1Nom13, infjug.eq1Nom14];
+            const nombresJugEq2=[infjug.eq2Nom1, infjug.eq2Nom2, infjug.eq2Nom3, infjug.eq2Nom4, infjug.eq2Nom5, infjug.eq2Nom6, infjug.eq2Nom7, infjug.eq2Nom8, infjug.eq2Nom9, infjug.eq2Nom10, infjug.eq2Nom11, infjug.eq2Nom12, infjug.eq2Nom13, infjug.eq2Nom14];
+            const numerosJugEq1=[infjug.eq1Num1, infjug.eq1Num2, infjug.eq1Num3, infjug.eq1Num4, infjug.eq1Num5, infjug.eq1Num6, infjug.eq1Num7, infjug.eq1Num8, infjug.eq1Num9, infjug.eq1Num10, infjug.eq1Num11, infjug.eq1Num12, infjug.eq1Num13, infjug.eq1Num14];
+            const numerosJugEq2=[infjug.eq2Num1, infjug.eq2Num2, infjug.eq2Num3, infjug.eq2Num4, infjug.eq2Num5, infjug.eq2Num6, infjug.eq2Num7, infjug.eq2Num8, infjug.eq2Num9, infjug.eq2Num10, infjug.eq2Num11, infjug.eq2Num12, infjug.eq2Num13, infjug.eq2Num14];
             
             for (let i=0; i < infjug.CantJugEq1; i++){
                 const valores= [nombresJugEq1[i], numerosJugEq1[i],idEquipo1];
